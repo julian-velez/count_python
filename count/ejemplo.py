4df47024bc2d9fbe69f2f2ae726941eea0625a29
julian-velez <60607869+julian-velez@users.noreply.github.com>
2025-08-03 17:59:46 -0500

    count_python
    count_python

---
+++ count/ejemplo.py
@@ -0,0 +1,7 @@
+# Se crea una variable llamada 'texto' que contiene una cadena de caracteres
+texto = 'julian Velez G'
+
+# Se utiliza el método .count() para contar cuántas veces aparece la letra 'j' en el texto
+# El método .count() distingue entre mayúsculas y minúsculas, así que solo cuenta letras 'j' minúsculas
+print(texto.count('j'))  # Esto imprimirá 1 porque solo hay una 'j' en el texto
+
